beb11b8f8b006f01af0f40d684340d864e6e58b2
Navin Kumar <navin@fplabs.tech>
2024-01-30 12:59:01 +0530

    Database Connection Changes

--- database/config.php
+++ database/config.php
@@ -1,7 +1,11 @@
 <?php
-define("DB_HOST", "localhost");
+define("DB_HOST", "127.0.0.1");
 define("DB_UNAME", "root");
 define("DB_PASS", "root");
 define("DB_DNAME", "quiz");
 $conn = mysqli_connect(DB_HOST, DB_UNAME, DB_PASS, DB_DNAME);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 ?>
